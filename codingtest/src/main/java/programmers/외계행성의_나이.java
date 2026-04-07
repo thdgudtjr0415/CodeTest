3d13e50ec7c277464acea7c59db17b53eadcdeda
SongHyungSeok <thdgudtjr0415@gmail.com>
2026-04-07 15:16:39 +0900

    20260407 코딩테스트

---
+++ codingtest/src/main/java/programmers/외계행성의_나이.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.util.Scanner;
+
+/*
+ * 우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다.
+ * 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는
+ * 나이를 알파벳으로 말하고 있습니다. a는 0, b는 1, c는 2, ..., j는 9입니다.
+ * 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때
+ *  PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
+ */
+
+
+public class 외계행성의_나이 {
+        public String solution(int age) {
+            String answer = "";
+            String age962 = "abcdefghij";
+            String[] ageArr = String.valueOf(age).split("");
+
+            for(int i = 0; i < ageArr.length; i++) {
+                answer += age962.charAt(Integer.valueOf(ageArr[i]));
+            }
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            외계행성의_나이 p = new 외계행성의_나이();
+            System.out.println(p.solution(23));
+            System.out.println(p.solution(51));
+            System.out.println(p.solution(100));
+        }
+}
