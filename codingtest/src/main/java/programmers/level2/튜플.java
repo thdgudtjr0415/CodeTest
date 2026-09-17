3343bcdcbf9abdc6e8ec78798a911861ff78cf2a
SongHyungSeok <thdgudtjr0415@gmail.com>
2026-09-17 11:58:06 +0900

    Coding Test2

--- codingtest/src/main/java/programmers/level2/튜플.java
+++ codingtest/src/main/java/programmers/level2/튜플.java
@@ -64,11 +64,5 @@ public class 튜플 {
         System.out.println(Arrays.toString(s.solution("{{20,111},{111}}"))); // 111,20
         System.out.println(Arrays.toString(s.solution("{{123}}"))); // 123
         System.out.println(Arrays.toString(s.solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))); // 3,2,4,1
-
-        System.out.println(Arrays.toString(s.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))); // 2,1,3,4
-        System.out.println(Arrays.toString(s.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))); // 2,1,3,4
-        System.out.println(Arrays.toString(s.solution("{{20,111},{111}}"))); // 111,20
-        System.out.println(Arrays.toString(s.solution("{{123}}"))); // 123
-        System.out.println(Arrays.toString(s.solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))); // 3,2,4,1
     }
 }
